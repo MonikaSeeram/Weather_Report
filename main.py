b0d2e794727476613ae0ba6ec1c4a5147bd231fc
Monika Seeram <monikas@ahaicoe.org>
2022-09-13 13:46:11 +0530

    modified

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os# To create a directory and to fetch the contents .
-from generating_weatherReport import* # imports all from the generating_weatherReport module
+from output import print # importing print function from output module
+from GeneratingReport import main # importing main function
 
-#Iterating and calling the main function which was imported from generating_weatherReport module
+
+#Iterating and calling the main function which was imported from GeneratingReport module
 choice = True
 while (choice):
     main()
@@ -11,7 +12,4 @@ while (choice):
     elif choice == 'no':
         choice = False
 
-weather.add_run("\n--------------- Thanks For Visiting 👋 ---------------")
-File_name = input("Enter the file name: ")
-doc.save(f'{File_name}.docx')  # saves the word document with output
-os.system(f'{File_name}.docx')# it directly open the document
+print() # calling the function to display the report on word document
